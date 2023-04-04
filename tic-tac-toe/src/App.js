e63aec6264c527b564b860e98d0f87cf4c8b3859
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2023-04-04 20:03:11 +0200

    updated Cell.js

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,15 +1,25 @@
+import { useState } from "react"
 import Cell from "./components/Cell"
 const App = () => {
-    return ( <
-        div className = "app" >
-        <
-        div className = "gameboard" >
-        <
-        Cell / >
-        <
-        /div> <
-        p > < /p> <
-        /div>
+    const [cells, setCells] = useState(["","","","","","","","",""])
+    const [go, setGo] = useState("circle")
+    const [winningMessage, setWinningMessage] = useState(null)
+    
+    const message = "it is now " + go + " 's go."
+    
+    return ( 
+        <div className = "app">
+            <div className = "gameboard">
+            {cells.map((cell, index) =>
+        <Cell 
+        key={index} 
+        id={index}
+        cell={cell}
+        setCells={setCells}
+    />)}
+        </div> 
+        <p>{message}</p> 
+    </div>
     )
 }
 
--- tic-tac-toe/src/components/Cell.js
+++ tic-tac-toe/src/components/Cell.js
@@ -1,4 +1,12 @@
-const Cell = () => {
-        return ( <div className = "square"> </div>) }
+const Cell = ({ id , cell, setCells }) => {
+    
+    const handleClick = (e) => {
+        const taken = e.target.firstChild.classList.contains("circle") ||
+              e.target.firstChild.classList.contains("cross")
+    }
+        return ( 
+            <div className = "square" id={id} onClick={handleClick}></div>
+    ) 
+}
 
-                export default Cell
+export default Cell
