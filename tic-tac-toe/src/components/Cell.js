a4966aebbc6ffd8b70cabf8d10f524f98c92be9c
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2023-04-16 11:02:11 +0200

    Fixed Cell.js

--- tic-tac-toe/src/components/Cell.js
+++ tic-tac-toe/src/components/Cell.js
@@ -1,9 +1,9 @@
-const Cell = ({ id , cell, setCells, go, setGo, cells, winningMessage }) => {
-    
+const Cell = ({ id, cell, setCells, go, setGo, cells, winningMessage }) => {
+
     const handleClick = (e) => {
         const taken = e.target.firstChild.classList.contains("circle") ||
-              e.target.firstChild.classList.contains("cross")
-        
+            e.target.firstChild.classList.contains("cross")
+
         if (!taken) {
             if (go === "circle") {
                 e.target.firstChild.classList.add("circle")
@@ -14,23 +14,26 @@ const Cell = ({ id , cell, setCells, go, setGo, cells, winningMessage }) => {
                 e.target.firstChild.classList.add("cross")
                 handleCellChange("cross")
                 setGo("circle")
+            }
         }
     }
-        const handleCellChange = (className) => {
-           const nextCells = cells.map((cell, index) => {
-                if (index === id) {
-                    return className
-                } else {
-                    return cell
-                }
-            })
-           setCells(nextCells)
-        }
-        
-        return ( 
-            <div className = "square" id={id} onClick={!winningMessage && handleClick}></div>
-                <div className={cell}></div>
-    ) 
+    const handleCellChange = (className) => {
+        const nextCells = cells.map((cell, index) => {
+            if (index === id) {
+                return className
+            } else {
+                return cell
+            }
+        })
+        setCells(nextCells)
+    }
+
+    return (
+        <React.Fragment>
+          <div className="square" id={id} onClick={!winningMessage && handleClick}></div>
+          <div className={cell}></div>
+       </React.Fragment>
+    )
 }
 
-export default Cell
+export default Cell;
